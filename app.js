7b62e238b4a0f4b833dfaffcc292958140ba4f78
ros002x <rosariotrupolo@gmail.com>
2026-08-30 10:06:49 +0200

    Show document share action on iPad

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ========================================
 // CONFIGURATION / VERSIONING
 // ========================================
-const APP_VERSION = "20260830-1025";
+const APP_VERSION = "20260830-1035";
 const APP_VERSION_KEY = "nsVolleyAppVersion";
 
 function enforceFreshAppVersion() {
